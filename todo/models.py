d72406bdab344f04768099e6c8d19d9d9fc8176b
lilychuang78 <lilychuang78@gmail.com>
2021-04-07 15:47:27 +0000

    model part2: create Item and display item name in admin page

--- todo/models.py
+++ todo/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(, null=False, blank=False, default=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # display the item name instead of 'item object x' in the admin page
+    def __str__(self):
+        return self.name
+
