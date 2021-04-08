d058720ba41af0e13eede6235380ee5196894007
lilychuang78 <lilychuang78@gmail.com>
2021-04-08 10:36:00 +0000

    add for loop, if statement and empty: you have nothing to do

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
@@ -8,4 +10,3 @@ class Item(models.Model):
     # display the item name instead of 'item object x' in the admin page
     def __str__(self):
         return self.name
-
