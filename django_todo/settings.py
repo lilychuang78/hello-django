960e8f52712cd4e947df1626d296547b47346b06
lilychuang78 <lilychuang78@gmail.com>
2021-04-23 12:57:17 +0000

    fixed os.environ.get in secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY', 'django-insecure-zv81_6ohpk#yexdo(j%01af+!c@)*xb&$se_bn+*#(&5or!q#7')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-zv81_6ohpk#yexdo(j%01af+!c@)*xb&$se_bn+*#(&5or!q#7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
